872545162dde3f3abe8e8a61ba02a551bdb9d9c6
Hannah Huang <yunhanh@bu.edu>
2020-11-09 16:46:29 -0500

    add components to signup page

---
+++ src/components/SignUpPage/SignUpStart.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SignUpStart = () => (
+    <div className="SignUp-bigBox">
+    <h1 className="sign-up-title">
+      $60 Offer - Get $20 Off Each of Your First 3 Boxes!
+    </h1>
+    <p className="sign-up-subtitle">
+      Enter your email address and password to get started
+    </p>
+    <div className="row">
+      <div className="col-12 col-md-6">
+      <h1>How it works</h1>
+      {/* three boxes down here */}
+      </div>
+      <div className="col-12 col-md-6">
+      <h1>Email</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default SignUpStart;
