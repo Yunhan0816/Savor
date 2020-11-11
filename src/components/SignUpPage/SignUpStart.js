c61878637f4da6d9c0fffcc682364adb50bf1f2b
Nathalie Ye <nathalieymy@gmail.com>
2020-11-11 18:43:05 +0100

    update styling

--- src/components/SignUpPage/SignUpStart.js
+++ src/components/SignUpPage/SignUpStart.js
@@ -13,15 +13,15 @@ const SignUpStart = () => (
 
       <div className="row">
 
-        <div className="col-12 col-md-6">
+        <div className="col-12 col-md-7">
           <h5 className="how-it-works">How it works</h5>
           {/* three boxes down here */}
         </div>
 
-        <div className="col-12 col-md-6">
-          <h5 className="login">Email</h5>
-          <input type="email"/>
-
+        <div className="col-12 col-md-3 email-container">
+          <h5 className="titleEmail">Email</h5>
+          <input className="inputEmail" type="email"/>
+          <button className="btnSignUp">Sign Up</button>
         </div>
 
       </div>
