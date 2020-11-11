e86f8691801c8f1279a3b9e4b4036c2dd97cea54
Nathalie Ye <nathalieymy@gmail.com>
2020-11-11 17:14:14 +0100

    updated sign up page and footer

--- src/components/SignUpPage/SignUpStart.js
+++ src/components/SignUpPage/SignUpStart.js
@@ -1,22 +1,31 @@
 import React from "react";
 
 const SignUpStart = () => (
-    <div className="SignUp-bigBox">
-    <h1 className="sign-up-title">
-      $60 Offer - Get $20 Off Each of Your First 3 Boxes!
-    </h1>
-    <p className="sign-up-subtitle">
-      Enter your email address and password to get started
-    </p>
-    <div className="row">
-      <div className="col-12 col-md-6">
-      <h1>How it works</h1>
-      {/* three boxes down here */}
+
+    // <div className="signup-container">
+    //   <h1 className="sign-up-title">$60 Offer - Get $20 Off Each of Your First 3 Boxes!</h1>
+    // </div>
+    <div className="signup-container">
+
+      <h2 className="sign-up-title">$60 Offer - Get $20 Off Each of Your First 3 Boxes!</h2>
+
+      <p className="sign-up-subtitle">Enter your email address and password to get started</p>
+
+      <div className="row">
+
+        <div className="col-12 col-md-6">
+          <h5 className="how-it-works">How it works</h5>
+          {/* three boxes down here */}
+        </div>
+
+        <div className="col-12 col-md-6">
+          <h5 className="login">Email</h5>
+          <input type="email"/>
+
+        </div>
+
       </div>
-      <div className="col-12 col-md-6">
-      <h1>Email</h1>
-      </div>
-    </div>
+
   </div>
 );
 
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,65 +2,47 @@ import React from "react";
 
 const Footer = () => (
   <section className="footer">
-    <div className="row ">
-    
+    <div className="row">
       <div className="col-12 col-md-3">
         <p>
-            <a href="#" target="_blank">Our Menu</a>
-          <a href="#" target="_blank">Plans</a>
-          <br />
-          <a href="#" target="_blank">
-            Gifts
-          </a>
-          <br />
-          <a href="#" target="_blank">Our Vision</a>
-          <br />
-          <a href="#" target="_blank">
-            Do Not "Sell" My Information
-          </a>
+          <a href="#" target="_blank">Our Menu</a><br/>
+          <a href="#" target="_blank">Plans</a><br/>          
+          <a href="#" target="_blank">Gifts</a><br/>          
+          <a href="#" target="_blank">Our Vision</a><br/>
+          <a href="#" target="_blank">Do Not "Sell" My Information</a>
         </p>
       </div>
 
       <div className="col-12 col-md-2">
-      <p>
-            <a href="#" target="_blank">Blog</a>
-            <br />
-          <a href="#" target="_blank">Recipes</a>
-          <br />
-          <a href="#" target="_blank">
-            Affiliates
-          </a>
-        
+        <p>
+          <a href="#" target="_blank">Blog</a><br/>
+          <a href="#" target="_blank">Recipes</a><br/>
+          <a href="#" target="_blank">Affiliates</a>
         </p> 
       </div>
+
       <div className="col-12 col-md-2">
-      <p>
-            <a href="#" target="_blank">Press</a>
-            <br />
-          <a href="#" target="_blank">Our Team</a>
-          <br />
-          <a href="#" target="_blank">
-            Investor Relations
-          </a>
-        
+        <p>
+          <a href="#" target="_blank">Press</a><br/>
+          <a href="#" target="_blank">Our Team</a><br/>
+          <a href="#" target="_blank">Investor Relations</a>
         </p> 
       </div>
+
       <div className="col-12 col-md-2">
-      <p>
-            <b href="#" target="_blank">Customer Support:</b>
-            <br />
-          <a href="#" target="_blank">Help Center & FAQ </a>
-          <br />
-          <a href="#" target="_blank">
-            contact@savor.com
-          </a>
-          <br/>
-          <a href="#" target="_blank">
-              (617) 384-3475
-          </a> 
+        <p>
+          <b href="#" target="_blank">Customer Support:</b><br/>
+          <a href="#" target="_blank">Help Center & FAQ </a><br/>
+          <a href="#" target="_blank">contact@savor.com</a><br/>
+          <p>(617) 384-3475</p> 
         </p> 
       </div>
+      
+    </div>
+
+    <div className="copyright">
+      <p>Copyright © 2020 SAVOR USA. All rights reserved.</p>
     </div>
   </section>
-);
+)
 export default Footer;
