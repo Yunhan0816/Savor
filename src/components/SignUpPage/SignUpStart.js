2a8e802fce6cf7421bb0b8168894d408febbc8f4
Nathalie Ye <nathalieymy@gmail.com>
2020-11-29 18:37:07 +0100

    sign up and login styling updated

--- src/components/SignUpPage/SignUpStart.js
+++ src/components/SignUpPage/SignUpStart.js
@@ -2,17 +2,37 @@ import React from "react";
 
 const SignUpStart = () => (
   
-    <div className="signup-container">
+    <div class="container" >
 
-      <h2 className="sign-up-title">$60 Offer - Get $20 Off Each of Your First 3 Boxes!</h2>
+      <h2 className="sign-up-title">Sign Up</h2>
 
-      <p className="sign-up-subtitle">Enter your email address and password to get started</p>
+      <p className="sign-up-subtitle">Sign up with your email or phone number and create a password to get started.</p>
 
-      <div className="row">
+      <form>
+        <div class="form-group"> 
+          <label for="email_input" class="titleEmail">Email</label>
+          <input type="email" class="form-control" id="email_input" ></input>
+        </div>
+        <div class="form-group">
+          <label for="phone_number_input" class="titlePhoneNumber">Phone Number</label>
+          <input type="number" class="form-control" id="phone_number_input" ></input>
+        </div>
+        <div class="form-group">
+          <label for="password_input" class="titlePassword">Password</label>
+          <input type="password" class="form-control" id="password_input" ></input>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="remember_me_check"></input>
+            <label class="form-check-label textRememberMe" for="remember_me_check">Remember Me</label>
+        </div>
+        <button type="submit" class="btnSignUp">Sign Up</button>
+      </form>
+
+      {/* <div className="row">
 
         <div className="col-12 col-md-7">
           <h5 className="how-it-works">How it works</h5>
-          {/* three boxes down here */}
+          three boxes down here
         </div>
 
         <div className="col-12 col-md-3 email-container">
@@ -21,7 +41,7 @@ const SignUpStart = () => (
           <button className="btnSignUp" > Sign Up </button>
         </div>
 
-      </div>
+      </div> */}
 
   </div>
 );
--- src/components/login.js
+++ src/components/login.js
@@ -33,8 +33,43 @@ class Login extends React.Component {
         }
 
         return (
-            <>
-                <h1>Log In</h1>
+            <><div class="container" >
+
+                <h2 className="sign-up-title">Log In</h2>
+                
+                <form method="post"
+                    onSubmit={event => {
+                        this.handleSubmit(event)
+                        navigate(`/profile`)
+                    }}>
+                    
+                    {/* email */}
+                    <div class="form-group"> 
+                        <label for="email_input" class="titleEmail">Email</label>
+                        <input type="email" class="form-control" id="email_input" ></input>
+                    </div>
+
+                    {/* password */}
+                    <div class="form-group">
+                        <label for="password_input" class="titlePassword">Password</label>
+                        <input type="password" class="form-control" id="password_input" ></input>
+                    </div>
+
+                    {/* remember me */}
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="remember_me_check"></input>
+                        <label class="form-check-label textRememberMe" for="remember_me_check">Remember Me</label>
+                    </div>
+
+                    {/* log in */}
+                    <button type="submit" class="btnLogin">Log In</button>
+                    <input type="submit" value="Log in with Google" />
+
+                </form>
+            </div>
+
+
+                {/* <h1>Log In</h1>
                 <form
                     method="post"
                     onSubmit={event => {
@@ -43,7 +78,7 @@ class Login extends React.Component {
                     }}
                 >
                     <input type="submit" value="Log in with Google" />
-                </form>
+                </form> */}
             </>
         )
     }
