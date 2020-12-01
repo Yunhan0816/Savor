6ea04a9ced598ee55d524587bff126a0dc4d94f5
Hannah Huang <yunhanh@bu.edu>
2020-11-30 23:06:09 -0500

    add firebase deploy

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,25 @@
-import {navigate} from "@reach/router";
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/firestore");
+// import {navigate} from "@reach/router";
+// const firebase = require("firebase/app");
+// require("firebase/auth");
+// require("firebase/firestore");
+import firebase from 'firebase/app'
+import 'firebase/database'
 
-const firebaseConfig = {
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBsPqwePo9hmTG3H_xCwpc534_ynne1IsU",
+//     authDomain: "savor-3e31c.firebaseapp.com",
+//     databaseURL: "https://savor-3e31c.firebaseio.com",
+//     projectId: "savor-3e31c",
+//     storageBucket: "savor-3e31c.appspot.com",
+//     messagingSenderId: "641219173895",
+//     appId: "1:641219173895:web:f897d37ebd34e5d78d14a2"
+// };
+
+// // Initialize Firebase
+// // firebase.initializeApp(firebaseConfig);
+// firebase.default.initializeApp(firebaseConfig);
+
+const config = {
     apiKey: "AIzaSyBsPqwePo9hmTG3H_xCwpc534_ynne1IsU",
     authDomain: "savor-3e31c.firebaseapp.com",
     databaseURL: "https://savor-3e31c.firebaseio.com",
@@ -11,46 +27,57 @@ const firebaseConfig = {
     storageBucket: "savor-3e31c.appspot.com",
     messagingSenderId: "641219173895",
     appId: "1:641219173895:web:f897d37ebd34e5d78d14a2"
-};
+  }
+  
+  let firebaseInstance
+  export const getFirebase = firebase => {
+    if (firebaseInstance) {
+      return firebaseInstance
+    }
+  
+    firebase.initializeApp(config)
+    firebaseInstance = firebase
+  
+    return firebase
+  }
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export const getUser = () => firebase.auth().currentUser;
 
-export const handleLogin = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const token = result.credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-    }).catch(function(error) {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        const credential = error.credential;
-    });
-}
+// export const getUser = () => firebase.default.auth().currentUser;
 
-export const isLoggedIn = () => {
-    const user = firebase.auth().currentUser
-    if (user === null) return false
-    else return true
-}
+// export const handleLogin = () => {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithPopup(provider).then(function(result) {
+//         // This gives you a Google Access Token. You can use it to access the Google API.
+//         const token = result.credential.accessToken;
+//         // The signed-in user info.
+//         const user = result.user;
+//         // ...
+//     }).catch(function(error) {
+//         // Handle Errors here.
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // The email of the user's account used.
+//         const email = error.email;
+//         // The firebase.auth.AuthCredential type that was used.
+//         const credential = error.credential;
+//     });
+// }
 
-export const logout = () => {
-    firebase.auth().signOut().then(_ => {
-        // Sign-out successful.
-        console.log("User was successfully logged out.")
-    });
-        navigate('/')
-        .catch(function(error) {
-        // An error happened.
-        console.log(`An error occurred when attempting to sign out. The error was: ${error.message}`);
-    });
-}
+// export const isLoggedIn = () => {
+//     const user = firebase.default.auth().currentUser
+//     if (user === null) return false
+//     else return true
+// }
+
+// export const logout = () => {
+//     firebase.default.auth().signOut().then(_ => {
+//         // Sign-out successful.
+//         console.log("User was successfully logged out.")
+//     });
+//         navigate('/')
+//         .catch(function(error) {
+//         // An error happened.
+//         console.log(`An error occurred when attempting to sign out. The error was: ${error.message}`);
+//     });
+// }
