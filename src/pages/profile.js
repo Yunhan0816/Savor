d2fe448ca9d41c63ce80cf57b76733832ca6aba2
Hannah Huang <yunhanh@bu.edu>
2020-12-04 15:32:12 -0500

    finish profile page setup

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -13,11 +13,40 @@ const App = () =>{
             <div className="home">
                 <section className="start-section">
                     <h1>
-This is User Profile Page
+                    Welcome!
                     </h1>
+                    <div className="outerBox">
+                        <h2>Please start your profile</h2>
+                        <div className="innerBox">
+                            <div className="row">
+                              <div className="col-12 col-md-4 ">
+                                  First Name
+                                  </div>    
+                                  <div className="col-12 col-md-4 ">
+                                      Last Name
+                                  </div>   
+                                  <div className="col-12 col-md-4 ">
+                                  Date of Birth
+                                  </div>  
+                                  </div>
+                            <div className="row">
+                            <div className="col-12 col-md-4 ">
+                                  Phone
+                                  </div>    
+                                  <div className="col-12 col-md-4 ">
+                                    Email
+                                  </div>   
+                                   
+                           </div>
+                        <div>How did you hear about us?</div>
+                        </div>
+                        <br />
+                    </div>
+                    
 
 
-                    <button><a href="/preferences">Start</a></button>
+
+                    <button><a href="/preferences">Next</a></button>
                 </section>
             </div>
         </div>
