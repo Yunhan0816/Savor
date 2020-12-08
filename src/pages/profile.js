603cb02c4c1ca95911ada08a4a9cbdb59a44faf5
Alex Doval <adoval18@gmail.com>
2020-12-08 18:05:17 -0500

    organized it into components

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,5 @@
 import Layout from "../components/layout";
+import ProfileStart from "../components/StartYourProfilePage/ProfileStart"
 import React from "react";
 
 export default () => (
@@ -12,41 +13,7 @@ const App = () =>{
         <div className="page-body">
             <div className="home">
                 <section className="start-section">
-                    <h1>
-                    Welcome!
-                    </h1>
-                    <div className="outerBox">
-                        <h2>Please start your profile</h2>
-                        <div className="innerBox">
-                            <div className="row">
-                              <div className="col-12 col-md-4 ">
-                                  First Name
-                                  </div>    
-                                  <div className="col-12 col-md-4 ">
-                                      Last Name
-                                  </div>   
-                                  <div className="col-12 col-md-4 ">
-                                  Date of Birth
-                                  </div>  
-                                  </div>
-                            <div className="row">
-                            <div className="col-12 col-md-4 ">
-                                  Phone
-                                  </div>    
-                                  <div className="col-12 col-md-4 ">
-                                    Email
-                                  </div>   
-                                   
-                           </div>
-                        <div>How did you hear about us?</div>
-                        </div>
-                        <br />
-                    </div>
-                    
-
-
-
-                    <button><a href="/subscription">Next</a></button>
+                    <ProfileStart/>
                 </section>
             </div>
         </div>
